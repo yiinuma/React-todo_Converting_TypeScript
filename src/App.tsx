9dfb38dc95b79ab0287434e0a4b2d578f688446f
yiinuma <y.iinuma211@gmail.com>
2022-03-03 11:32:02 +0900

    inital setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-wrap justify-items-center bg-green-200 p-4">
+      <div className="m-4 flex h-80 w-80 items-center justify-center rounded-lg bg-green-400 text-9xl">1</div>
+      <div className="m-4 flex h-80 w-80 items-center justify-center rounded-lg bg-green-400 text-9xl">2</div>
+      <div className="m-4 flex h-80 w-80 items-center justify-center rounded-lg bg-green-400 text-9xl">3</div>
+      <div className="m-4 flex h-80 w-80 items-center justify-center rounded-lg bg-green-400 text-9xl">4</div>
+      <div className="m-4 flex h-80 w-80 items-center justify-center rounded-lg bg-green-400 text-9xl">5</div>
+      <div className="m-4 flex h-80 w-80 items-center justify-center rounded-lg bg-green-400 text-9xl">6</div>
+      <div className="m-4 flex h-80 w-80 items-center justify-center rounded-lg bg-green-400 text-9xl">7</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
